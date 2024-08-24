59df190f8a1675cf2462642151ea165448a68199
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2024-08-24 20:43:02 +0530

    Update songs.js

--- plugins/songs.js
+++ plugins/songs.js
@@ -1,6 +1,6 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
-const yts = require('yt-search)
+const yts = require('yt-search')
                     
 cmd({ 
     pattern: "song",
