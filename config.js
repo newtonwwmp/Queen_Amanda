15151ca42d0e0fd7771029a603e594bfe41aff38
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2024-08-25 08:03:57 +0530

    QUEEN-AMANDA

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello,I am Queen Amanda Whatsapp bot i am alive now*",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
