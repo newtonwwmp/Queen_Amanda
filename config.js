4fd5e2eccebe36ca4cd1cbc327d9a20be427a32c
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2024-08-25 17:19:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/xq615zVM/photo-2024-08-24-11-54-23.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello,I am Queen Amanda Whatsapp bot i am alive now*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: prcess.env.MODE || "public",
