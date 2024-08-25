0593063b1f829f55f037220e7a4ee295d9914fc3
MAHIYA__BOTS <161313291+newtonwwmp@users.noreply.github.com>
2024-08-25 09:06:29 +0530

    QUEEN-AMANDA

--- index.js
+++ index.js
@@ -165,8 +165,10 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
-//============================================================================ 
-
+//================================ work type ============================================ 
+if(!isowner && config.MODE === "Private") return
+if(!isowner && isGroup && config.MODE === "inbox") return
+if(!isowner && !isGroup && config.MODE === "groups") return
 })
 }
 app.get("/", (req, res) => {
